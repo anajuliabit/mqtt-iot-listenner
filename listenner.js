1341c5f6328baf0d94947240ece63ace219e4510
Ana Julia Bittencourt <anajuliabit@gmail.com>
2020-09-05 11:59:57 -0300

    feat: listenner

---
+++ listenner.js
@@ -0,0 +1,36 @@
+const mqtt = require('mqtt')
+const fs = require('fs');
+
+var stream = {};
+
+var client  = mqtt.connect('mqtt://tnt-iot.maratona.dev',{
+  username: 'maratoners',
+  password: 'ndsjknvkdnvjsbvj',
+  port: '30573',
+  host: 'tnt-iot.maratona.dev'
+});
+
+process.on('SIGINT', function() {
+  console.log("Interrompendo gravação...");
+  process.exit();
+});
+
+const SAVE_DATA = function(STRING){
+  fs.appendFile('./aquisition/TNT_DATA.csv', STRING, function (err) {
+    if (err) throw err;
+    console.log('@Saved@\n');
+  });
+};
+
+client.on('connect', function () {
+
+  console.log('Client connected');
+    client.subscribe('tnt', function (err) {
+      client.on('message', function(topic, message, packet) {
+          let TNT_DATA = JSON.parse(message);
+          console.log('#TNT_DATA: '+message);
+          SAVE_DATA(`${TNT_DATA['row']},${TNT_DATA['Tempo']},${TNT_DATA['Estação']},${TNT_DATA['LAT']},${TNT_DATA['LONG']},${TNT_DATA['Movimentação']},${TNT_DATA['Original_473']},${TNT_DATA['Original_269']},${TNT_DATA['Zero']},${TNT_DATA['Maçã-Verde']},${TNT_DATA['Tangerina']},${TNT_DATA['Citrus']},${TNT_DATA['Açaí-Guaraná']},${TNT_DATA['Pêssego']},${TNT_DATA['TARGET']}\n`);
+      });
+    });
+
+});
